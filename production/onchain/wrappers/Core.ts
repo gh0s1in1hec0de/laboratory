f4949088920a584b466eeee251ff5aa221b8a41d
Александр Крюков <dondarubin@gmail.com>
2024-08-13 00:06:37 +0400

    write core wrapper, recalc sizes in main.tlb and jetton.tlb

--- production/onchain/wrappers/Core.ts
+++ production/onchain/wrappers/Core.ts
@@ -1,85 +1,97 @@
-import {Ops} from "./utils";
+import {OpCore, TEP64JettonMetadata, TEP64MetadataToCell} from "./utils";
 import {
-  ContractProvider,
-  contractAddress,
-  Dictionary,
-  beginCell,
-  Contract,
-  SendMode,
   Address,
-  Sender,
+  beginCell,
   Cell,
+  Contract,
+  contractAddress,
+  ContractProvider,
+  Dictionary,
+  Sender,
+  SendMode,
 } from "@ton/core";
 
-// Replace `;` with `,`
 export type CoreConfig = {
-  chief: Address;
-  utilJettonMasterAddress: Address;
-  utilJettonWalletAddress: Address;
-  utilJetCurBalance: bigint;
-  notFundedLaunches: Dictionary<Address, Cell> | null;
-  notFundedLaunchesAmount: number;
+  chief: Address,
+  utilJettonMasterAddress: Address,
+  utilJettonWalletAddress: Address,
+  utilJetCurBalance: bigint,
+  notFundedLaunches: Dictionary<Address, Cell> | null,
+  notFundedLaunchesAmount: number,
   launchConfig: {
-    minTonForSaleSuccess: bigint;
-    tonLimitForWlRound: bigint;
-    utilJetRewardAmount: bigint;
-    utilJetWlPassAmount: bigint;
-    utilJetBurnPerWlPassAmount: bigint;
-    jetWlLimitPct: number;
-    jetPubLimitPct: number;
-    jetDexSharePct: number;
-    creatorRoundDurationMs: number;
-    wlRoundDurationMs: number;
-    pubRoundDurationMs: number;
+    minTonForSaleSuccess: bigint,
+    tonLimitForWlRound: bigint,
+    utilJetRewardAmount: bigint,
+    utilJetWlPassAmount: bigint,
+    utilJetBurnPerWlPassAmount: bigint,
+    jetWlLimitPct: number,
+    jetPubLimitPct: number,
+    jetDexSharePct: number,
+    creatorRoundDurationMs: number,
+    wlRoundDurationMs: number,
+    pubRoundDurationMs: number,
   };
   contracts: {
-    jettonLaunch: Cell;
-    jettonLaunchUserVault: Cell;
-    derivedJettonMaster: Cell;
-    jettonWallet: Cell;
+    jettonLaunch: Cell,
+    jettonLaunchUserVault: Cell,
+    derivedJettonMaster: Cell,
+    jettonWallet: Cell,
   };
 };
 
 export function coreConfigToCell(config: CoreConfig): Cell {
   const contractsCell = beginCell()
-  .storeRef(config.contracts.jettonLaunch)
-  .storeRef(config.contracts.jettonLaunchUserVault)
-  .storeRef(config.contracts.derivedJettonMaster)
-  .storeRef(config.contracts.jettonWallet)
-  .endCell();
+    .storeRef(config.contracts.jettonLaunch)
+    .storeRef(config.contracts.jettonLaunchUserVault)
+    .storeRef(config.contracts.derivedJettonMaster)
+    .storeRef(config.contracts.jettonWallet)
+    .endCell();
 
   const launchConfigCell = beginCell()
-  .storeCoins(config.launchConfig.minTonForSaleSuccess)
-  .storeCoins(config.launchConfig.tonLimitForWlRound)
-  .storeCoins(config.launchConfig.utilJetRewardAmount)
-  .storeCoins(config.launchConfig.utilJetWlPassAmount)
-  .storeCoins(config.launchConfig.utilJetBurnPerWlPassAmount)
-  .storeUint(config.launchConfig.jetWlLimitPct, 16)
-  .storeUint(config.launchConfig.jetPubLimitPct, 16)
-  .storeUint(config.launchConfig.jetDexSharePct, 16)
-  .storeInt(config.launchConfig.creatorRoundDurationMs, 32)
-  .storeInt(config.launchConfig.wlRoundDurationMs, 32)
-  .storeInt(config.launchConfig.pubRoundDurationMs, 32)
-  .endCell();
+    .storeCoins(config.launchConfig.minTonForSaleSuccess)
+    .storeCoins(config.launchConfig.tonLimitForWlRound)
+    .storeCoins(config.launchConfig.utilJetRewardAmount)
+    .storeCoins(config.launchConfig.utilJetWlPassAmount)
+    .storeCoins(config.launchConfig.utilJetBurnPerWlPassAmount)
+    .storeUint(config.launchConfig.jetWlLimitPct, 16)
+    .storeUint(config.launchConfig.jetPubLimitPct, 16)
+    .storeUint(config.launchConfig.jetDexSharePct, 16)
+    .storeInt(config.launchConfig.creatorRoundDurationMs, 32)
+    .storeInt(config.launchConfig.wlRoundDurationMs, 32)
+    .storeInt(config.launchConfig.pubRoundDurationMs, 32)
+    .endCell();
 
   return beginCell()
-  .storeAddress(config.chief)
-  .storeAddress(config.utilJettonMasterAddress)
-  .storeAddress(config.utilJettonWalletAddress)
-  .storeCoins(config.utilJetCurBalance)
-  .storeDict(config.notFundedLaunches)
-  .storeUint(config.notFundedLaunchesAmount, 8)
-  .storeRef(launchConfigCell)
-  .storeRef(contractsCell)
-  .endCell();
+    .storeAddress(config.chief)
+    .storeAddress(config.utilJettonMasterAddress)
+    .storeAddress(config.utilJettonWalletAddress)
+    .storeCoins(config.utilJetCurBalance)
+    .storeDict(config.notFundedLaunches)
+    .storeUint(config.notFundedLaunchesAmount, 8)
+    .storeRef(launchConfigCell)
+    .storeRef(contractsCell)
+    .endCell();
 }
 
-export type CreateLaunchParams =  {
-  startTime: number // Unix timestamp
+export type CreateLaunchParams = {
+  startTime: number, // Unix timestamp
   totalSupply: bigint | number,
   platformSharePct: number,
-  metadata: Cell,
+  metadata: Cell | TEP64JettonMetadata,
 };
+
+export type TransferNotificationParams = {
+  jettonAmount: bigint | number,
+  initiator: Address,
+  maybePayload: Cell
+}
+
+export type UpgradeParams = {
+  newData2: number,
+  newData3: Address,
+  newData: Cell,
+  newCode: Cell,
+}
 
 export class Core implements Contract {
   constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {
@@ -99,20 +111,59 @@ export class Core implements Contract {
     await provider.internal(via, {
       value,
       sendMode: SendMode.PAY_GAS_SEPARATELY,
-      body: beginCell().storeUint(Ops.core_init, 32).storeUint(0, 64).endCell(),
+      body: beginCell().storeUint(OpCore.init, 32).storeUint(0, 64).endCell(),
     });
   }
 
-  // To build metadata see ./utils#L16
   async sendCreateLaunch(provider: ContractProvider, via: Sender, value: bigint, queryId: bigint, params: CreateLaunchParams) {
-   const body = beginCell()
-       .storeUint(Ops.create_launch, 32)
-       .storeUint(queryId, 64)
-       .storeInt(params.startTime, 32)
-       .storeCoins(params.totalSupply)
-       .storeUint(params.platformSharePct, 16)
-       .storeRef(params.metadata)
-       .endCell();
+    const {startTime, totalSupply, platformSharePct, metadata} = params;
+    const packagedMetadata = metadata instanceof Cell ? metadata : TEP64MetadataToCell(metadata);
+    const body = beginCell()
+      .storeUint(OpCore.create_launch, 32)
+      .storeUint(queryId, 64)
+      .storeInt(startTime, 32)
+      .storeCoins(totalSupply)
+      .storeUint(platformSharePct, 16)
+      .storeRef(packagedMetadata)
+      .endCell();
+    await provider.internal(via, {
+      value, sendMode: SendMode.PAY_GAS_SEPARATELY, body
+    });
+  }
+
+  async sendTlInitCallback(provider: ContractProvider, via: Sender, value: bigint, queryId: bigint) {
+    const body = beginCell()
+      .storeUint(OpCore.tl_init_callback, 32)
+      .storeUint(queryId, 64)
+      .endCell();
+    await provider.internal(via, {
+      value, sendMode: SendMode.PAY_GAS_SEPARATELY, body
+    });
+  }
+
+  // TODO figure out type of maybePayload and how to pass on (Either Cell ^Cell)
+  async sendTransferNotification(provider: ContractProvider, via: Sender, value: bigint, queryId: bigint, params: TransferNotificationParams) {
+    const {jettonAmount, initiator, maybePayload} = params;
+    const body = beginCell()
+      .storeUint(OpCore.transfer_notification, 32)
+      .storeUint(queryId, 64)
+      .storeCoins(jettonAmount)
+      .storeAddress(initiator)
+      .storeRef(maybePayload)
+      .endCell();
+    await provider.internal(via, {
+      value, sendMode: SendMode.PAY_GAS_SEPARATELY, body
+    });
+  }
+
+  async sendUpgrade(provider: ContractProvider, via: Sender, value: bigint, queryId: bigint, params: UpgradeParams) {
+    const {newData, newCode} = params;
+    const body = beginCell()
+      .storeUint(OpCore.upgrade, 32)
+      .storeUint(queryId, 64)
+      .storeRef(newData)
+      .storeRef(newCode)
+      .endCell();
     await provider.internal(via, {
       value, sendMode: SendMode.PAY_GAS_SEPARATELY, body
     });
--- production/onchain/wrappers/utils.ts
+++ production/onchain/wrappers/utils.ts
@@ -1,8 +1,15 @@
 import {beginCell, Cell} from "@ton/core";
 
-export enum Ops {
-    core_init = 0x18add407,
+export enum OpCore {
+    init = 0x18add407,
     create_launch = 0x0eedbf42,
+    tl_init_callback = 0x5d988a60,
+    transfer_notification = 0x7362d09c,
+    upgrade = 0x055d212a,
+}
+
+export enum OpTokenLaucher {
+  upgrade = OpCore.upgrade,
 }
 
 export enum Errors {}
