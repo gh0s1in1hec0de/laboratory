dac09babfc67d1d781cabb1800bb260ea9e46193
gh0s1in1hec0de <kittenbillionaire@gmail.com>
2024-08-15 17:19:52 +0400

    docker-compose compressed and improved + minor changes

--- production/backend/src/db/db.ts
+++ production/backend/src/db/db.ts
@@ -5,11 +5,11 @@ import * as fs from "fs";
 
 export async function createPostgresClient(): Promise<Client> {
     return postgres({
-        host: process.env.DB_HOST,
+        host: "db",
         port: 5432,
-        database: process.env.DB_NAME,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
         types: {bigint: postgres.BigInt},
         transform: postgres.camel
     });
---
+++ production/backend/src/oracle/actors.ts
@@ -0,0 +1,5 @@
+import {TonClient4} from "@ton/ton";
+
+const tonClient = new TonClient4({
+    endpoint: ""
+});
