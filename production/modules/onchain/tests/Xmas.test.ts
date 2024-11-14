d24e4cf9e2708d591335bac5912fc62d22a8b3b9
gh0s1in1hec0de <kittenbillionaire@gmail.com>
2024-11-14 13:07:46 +0400

    xmas v1.666^^

--- production/modules/onchain/tests/Xmas.test.ts
+++ production/modules/onchain/tests/Xmas.test.ts
@@ -2,7 +2,9 @@ import { findTransactionRequired, randomAddress } from "@ton/test-utils";
 import { XmasJettonMaster } from "../wrappers/XmasJettonMaster";
 import { XmasJettonWallet } from "../wrappers/XmasJettonWallet";
 import { JettonOps, jettonToNano } from "starton-periphery";
+import { printTxGasStats } from "./utils/gasUtils";
 import { Address, Cell, toNano } from "@ton/core";
+import { ok as assert } from "node:assert";
 import { compile } from "@ton/blueprint";
 import "@ton/test-utils";
 import {
@@ -10,15 +12,22 @@ import {
     SandboxContract,
     Blockchain,
 } from "@ton/sandbox";
-import { printTxGasStats } from "./utils/gasUtils";
+
+// Event boundaries – update before testing.
+// Use `determine event timings` for convenience.
+const START = 1731743141;
+const END = 1732175141;
 
 describe("Marry Christmas and happy New Year!", () => {
+    let blockchain: Blockchain;
     let jettonMasterCode = new Cell();
     let jettonWalletCode = new Cell();
-    let blockchain: Blockchain;
+
     let deployer: SandboxContract<TreasuryContract>;
     let deployerWallet: SandboxContract<XmasJettonWallet>;
     let jettonMaster: SandboxContract<XmasJettonMaster>;
+
+    let userWallet: (owner: Address) => SandboxContract<XmasJettonWallet>;
 
     beforeAll(async () => {
         jettonMasterCode = await compile("XmasJettonMaster");
@@ -39,48 +48,150 @@ describe("Marry Christmas and happy New Year!", () => {
                 },
                 jettonMasterCode)
         );
-        deployerWallet = blockchain.openContract(
+        console.info(`Jetton master address: ${jettonMaster.address}`);
+
+        userWallet = (ownerAddress) => blockchain.openContract(
             XmasJettonWallet.createFromConfig(
-                {
-                    jettonMasterAddress: jettonMaster.address,
-                    ownerAddress: deployer.address
-                },
+                { jettonMasterAddress: jettonMaster.address, ownerAddress },
                 jettonWalletCode
             )
         );
+        deployerWallet = userWallet(deployer.address);
 
     }, 20000);
-    test.skip("now", async () => console.info(blockchain.now! + 86400, blockchain.now! + 7 * 86400));
-    test("minting jettons", async () => {
-        const mintResult = await jettonMaster.sendMint(
-            deployer.getSender(), deployer.address, jettonToNano("1000000")
-        );
-        expect(mintResult.transactions).toHaveTransaction({
-            op: JettonOps.InternalTransfer,
-            on: deployerWallet.address,
-            success: true,
-            deploy: true
-        });
-    }, 20000);
-    test("common transfer specs", async () => {
-        const transferResult = await deployerWallet.sendTransfer(
-            deployer.getSender(), toNano("1"), jettonToNano("100"), randomAddress(), deployer.address,
-            null, toNano("0.1"), null
-        );
 
-        const transferTx = findTransactionRequired(transferResult.transactions, {
-            op: JettonOps.Transfer,
-            success: true,
-        });
-        printTxGasStats("Transfer request transaction", transferTx);
-        expect(transferResult.transactions).not.toHaveTransaction({
-            op: JettonOps.IncreaseSupply,
-            success: true
-        });
-        const internalTransferTx = findTransactionRequired(transferResult.transactions, {
-            op: JettonOps.InternalTransfer,
-            success: true,
-        });
-        printTxGasStats("Internal transfer transaction", internalTransferTx);
-    }, 20000);
+    // Change constants to actual values before testing
+    test.skip("determine event timings", () => console.info(blockchain.now! + 2 * 86400, blockchain.now! + 7 * 86400));
+
+    describe("standard functions", () => {
+        test("minting jettons", async () => {
+            const mintResult = await jettonMaster.sendMint(
+                deployer.getSender(), deployer.address, jettonToNano("1000000")
+            );
+            expect(mintResult.transactions).toHaveTransaction({
+                op: JettonOps.InternalTransfer,
+                on: deployerWallet.address,
+                success: true,
+                deploy: true
+            });
+        }, 20000);
+        test("common transfer specs", async () => {
+            const recipient = randomAddress();
+            const transferAmount = jettonToNano("100");
+            const transferResult = await deployerWallet.sendTransfer(
+                deployer.getSender(), toNano("1"), transferAmount, recipient, deployer.address,
+                null, toNano("0.1"), null
+            );
+
+            const transferTx = findTransactionRequired(transferResult.transactions, {
+                op: JettonOps.Transfer,
+                success: true,
+            });
+            printTxGasStats("Transfer request transaction", transferTx);
+            expect(transferResult.transactions).not.toHaveTransaction({
+                op: JettonOps.IncreaseSupply,
+                success: true
+            });
+            const internalTransferTx = findTransactionRequired(transferResult.transactions, {
+                op: JettonOps.InternalTransfer,
+                success: true,
+            });
+            printTxGasStats("Internal transfer transaction", internalTransferTx);
+
+            // As the event hasn't been started yest
+            const walletBalance = await userWallet(recipient).getJettonBalance();
+            assert(walletBalance === transferAmount);
+        }, 20000);
+    });
+    describe("event!", () => {
+        // TODO Supply increase
+        test("christmas transfer", async () => {
+            blockchain.now = START + 1;
+
+            const supplyBefore = await jettonMaster.getTotalSupply();
+
+            const recipient = randomAddress();
+            const transferAmount = jettonToNano("100");
+            const transferResult = await deployerWallet.sendTransfer(
+                deployer.getSender(), toNano("1"), transferAmount, recipient, deployer.address,
+                null, toNano("0.1"), null
+            );
+
+            const transferTx = findTransactionRequired(transferResult.transactions, {
+                op: JettonOps.Transfer,
+                success: true,
+            });
+            printTxGasStats("Transfer request transaction", transferTx);
+            const increaseSupplyTx = findTransactionRequired(transferResult.transactions, {
+                op: JettonOps.IncreaseSupply,
+                success: true,
+            });
+            printTxGasStats("Supply increase transaction", increaseSupplyTx);
+            const internalTransferTx = findTransactionRequired(transferResult.transactions, {
+                op: JettonOps.InternalTransfer,
+                success: true,
+            });
+            printTxGasStats("Internal transfer transaction", internalTransferTx);
+
+            // As the event hasn't been started yest
+            const walletBalance = await userWallet(recipient).getJettonBalance();
+            const supplyAfter = await jettonMaster.getTotalSupply();
+
+            assert(walletBalance > transferAmount);
+            assert(supplyAfter === (supplyBefore + walletBalance - transferAmount));
+        }, 20000);
+
+        test("christmas transfers' statistics", async () => {
+            const increaseCounts = { "2%": 0, "4%": 0, "7%": 0, "10%": 0, "15%": 0 };
+            const totalTransfers = 100;
+
+            for (let i = 0; i < totalTransfers; i++) {
+                blockchain.now! += 1;
+                const recipient = randomAddress();
+                const transferAmount = jettonToNano("100");
+                const transferResult = await deployerWallet.sendTransfer(
+                    deployer.getSender(), toNano("1"), transferAmount, recipient, deployer.address,
+                    null, toNano("0.1"), null
+                );
+                expect(transferResult.transactions).toHaveTransaction({
+                    op: JettonOps.InternalTransfer,
+                    success: true,
+                });
+
+                const walletBalance = await userWallet(recipient).getJettonBalance();
+                const increaseAmount = walletBalance - transferAmount;
+                const increasePercent = Math.round((Number(increaseAmount) / Number(transferAmount)) * 100);
+
+                increaseCounts[`${increasePercent}%` as keyof typeof increaseCounts]++;
+            }
+
+            console.log("Increase type percentages: ");
+            for (const [type, count] of Object.entries(increaseCounts)) {
+                const percentage = (count / totalTransfers) * 100;
+                console.log(`${type} increase: ${percentage.toFixed(2)}%`);
+            }
+        }, 20000);
+        test("event end", async () => {
+            blockchain.now = END + 1;
+            const recipient = randomAddress();
+            const transferAmount = jettonToNano("100");
+            const transferResult = await deployerWallet.sendTransfer(
+                deployer.getSender(), toNano("1"), transferAmount, recipient, deployer.address,
+                null, toNano("0.1"), null
+            );
+
+            expect(transferResult.transactions).not.toHaveTransaction({
+                op: JettonOps.IncreaseSupply,
+                success: true
+            });
+            expect(transferResult.transactions).toHaveTransaction({
+                op: JettonOps.InternalTransfer,
+                success: true,
+            });
+
+            // As the event has ended
+            const walletBalance = await userWallet(recipient).getJettonBalance();
+            assert(walletBalance === transferAmount);
+        }, 20000);
+    });
 });
