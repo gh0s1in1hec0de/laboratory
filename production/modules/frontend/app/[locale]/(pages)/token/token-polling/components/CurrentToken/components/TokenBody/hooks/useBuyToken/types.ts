ad3c499f8746364016bdfa6a6be62913fd8108a9
dondarubin <dondarubin@gmail.com>
2024-11-28 16:52:35 +0400

    minor

--- production/modules/frontend/app/[locale]/(pages)/token/token-polling/components/CurrentToken/components/TokenBody/hooks/useBuyToken/types.ts
+++ production/modules/frontend/app/[locale]/(pages)/token/token-polling/components/CurrentToken/components/TokenBody/hooks/useBuyToken/types.ts
@@ -1,9 +1,8 @@
-import { TokenLaunchTimings, GlobalVersions, SalePhase } from "starton-periphery";
+import { TokenLaunchTimings, GlobalVersions } from "starton-periphery";
 
 export interface UseBuyTokenProps {
   supply: bigint;
   launchAddress: string;
   timings: TokenLaunchTimings;
   version: GlobalVersions;
-  phase: SalePhase;
 }
