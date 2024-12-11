46ea8cb9cc83cf416569c89d183f9c33a9a45a37
dondarubin <dondarubin@gmail.com>
2024-12-11 18:42:48 +0400

    add isBigBoss check

--- production/modules/frontend/app/[locale]/(pages)/token/components/CreateTokenForm/components/SubmitButton/components/CreateTokenButton/CreateTokenButton.tsx
+++ production/modules/frontend/app/[locale]/(pages)/token/components/CreateTokenForm/components/SubmitButton/components/CreateTokenButton/CreateTokenButton.tsx
@@ -4,10 +4,21 @@ import { useFormikContext } from "formik";
 import { useTranslations } from "next-intl";
 import { CREATE_TOKEN_FORM_ID } from "../../../../constants";
 import { CreateTokenFormFields } from "../../../../hooks/useCreateToken";
+import { Caller } from "starton-periphery";
 
-export function CreateTokenButton() {
+export function CreateTokenButton({ callerData }: { callerData: Caller | null }) {
   const t = useTranslations("Token.submitButton");
   const { errors, isSubmitting } = useFormikContext<CreateTokenFormFields>();
+
+  const isDisabled =
+    isSubmitting || 
+    (process.env.NEXT_PUBLIC_CREATOR_MODE === "private" && (!callerData || callerData.isBigBoss === false));
+
+  const labelText = isSubmitting
+    ? t("loading")
+    : isDisabled
+      ? t("disableText")
+      : t("label");
 
   return (
     <CustomButton
@@ -15,11 +26,11 @@ export function CreateTokenButton() {
       type="submit"
       padding="10px 6px"
       // disabled={Object.keys(errors).length > 0 || isSubmitting}
-      disabled={isSubmitting}
+      disabled={isDisabled}
       fullWidth
     >
       <Label
-        label={isSubmitting ? t("loading") : t("label")}
+        label={labelText}
         variantSize="medium16"
         offUserSelect
       />
