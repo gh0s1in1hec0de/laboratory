26c04859b9820c45481a0603a6a240be1ecb7a49
gh0s1in1hec0de <kittenbillionaire@gmail.com>
2024-12-15 20:01:19 +0400

    meow

--- production/modules/oracle/src/server/routes/tokenLaunch/index.ts
+++ production/modules/oracle/src/server/routes/tokenLaunch/index.ts
@@ -30,7 +30,6 @@ export function TokenLaunchRoutes() {
                 try {
                     return await getCertainLaunch(query);
                 } catch (e) {
-                    console.log(e);
                     if (e instanceof CommonServerError) return error(e.code, e.message);
                     else return error(500, e);
                 }
